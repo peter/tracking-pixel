5c7f7b3a54907c196b5484357f0b10c85bf7cc81
Peter Marklund <peter@marklunds.com>
2022-06-24 08:04:02 +0200

    write request to db, set cookie only if not set, use UUID

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 
 ## TODO
 
-* Add tracking pixel route
-* CORS headers?
-* MongoDB aggregations
 * Referrer
   https://developer.chrome.com/blog/referrer-policy-new-chrome-default/#:~:text=%23%20What%20does%20this%20change%20mean,the%20path%20and%20query%20string.
   https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy#integration_with_html
@@ -13,8 +10,8 @@
   Set-Cookie: _track=2022-06-22T19%3A16%3A16.816Z; Max-Age=43200; Path=/; Expires=Thu, 23 Jun 2022 07:16:16 GMT; HttpOnly
 * Log requests
 * CLI
+  MongoDB aggregations
 * Unit tests
-* Test HTML pages?
 * TypeScript?
 * What are the interesting considerations in this app?
 
@@ -23,10 +20,11 @@
 You need to have [MongoDB installed](https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-os-x/) or set the `MONGO_URL` env variable to connect to a hosted MongoDB service to run this app.
 
 ```sh
+nvm use
 npm install
 npm run dev
 
-curl -i http://localhost:3000/tracking
+curl -i -o /tmp/tracking-pixel.gif http://localhost:3000/tracking
 ```
 
 Test HTML page:
@@ -34,4 +32,11 @@ Test HTML page:
 ```sh
 npm run dev-static
 open http://localhost:8080/test/page1.html
+```
+
+Checking tracking events in the database:
+
+```sh
+npm run dev-db
+db.trackingEvents.find().sort({ timestamp: -1 }).pretty()
 ```
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const { MongoClient } = require('mongodb');
+const uuid = require('uuid')
+const { get } = require('lodash')
 
 const port = 3000
 const app = express()
+let db
 
 app.use(cors())
 app.use(cookieParser())
@@ -12,22 +15,16 @@ app.use(cookieParser())
 const LOG_LEVEL = process.env.LOG_LEVEL || 'info'
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/tracking-pixel'
 
-const PIXEL_IMAGE = Buffer.from('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64')
-
-const TEST_DATA = [
-  { timestamp: new Date('2013-09-01 09:00:00'), url: '/contact.html', userId: 12345 },
-  { timestamp: new Date('2013-09-01 09:00:00'), url: '/contact.html', userId: 12346  },
-  { timestamp: new Date('2013-09-01 10:00:00'), url: '/contact.html', userId: 12345  },
-  { timestamp: new Date('2013-09-01 10:01:00'), url: '/about.html', userId: 12347  },
-  { timestamp: new Date('2013-09-01 11:00:00'), url: '/contact.html', userId: 12347  },
-]
+const COOKIE_NAME = '_track'
+const PIXEL_IMAGE_BASE64 = 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+const COLLECTION_NAME = 'trackingEvents'
 
 function log(message, data = {}) {
   const LOG_LEVELS = ['debug', 'info', 'warning', 'error'];
   const level = data.level || 'info';
   if (!LOG_LEVELS.includes(level)) throw new Error(`invalid log level: ${level}`);
   if (LOG_LEVELS.indexOf(level) < LOG_LEVELS.indexOf(LOG_LEVEL)) return;
-  const timestamp = new Date().toISOString();
+  const timestamp = (data.timestamp || new Date()).toISOString();
   // eslint-disable-next-line no-console
   console.log(
     JSON.stringify({
@@ -39,34 +36,65 @@ function log(message, data = {}) {
   );
 }
 
+function logError(message, data = {}) {
+  log(message, { ...data, level: 'error' })
+}
+
 app.get('/tracking', (req, res) => {
-  log('tracking request', { referer: req.get('Referer'), cookies: req.cookies })
-  const cookieValue = new Date().toISOString()
-  res.cookie('_track', cookieValue, { maxAge: (1000 * 60 * 24 * 30), httpOnly: true });
+  const timestamp = new Date()
+  const url = req.get('Referer')
+  const requestUserId = get(req.cookies, COOKIE_NAME)
+  const userId = requestUserId || uuid.v4()
+  const firstRequest = !requestUserId
+  log('tracking request', { timestamp, url, userId, firstRequest })
+
+  // Save request in database
+  try {
+    db.collection(COLLECTION_NAME).insertOne({ timestamp, url, userId })
+  } catch (err) {
+    logError('error thrown saving to db', { timestamp, url, userId });
+  }
+
+  // Set cookie in response if not already set
+  if (!requestUserId) {
+    res.cookie(COOKIE_NAME, userId, {
+      maxAge: (1000 * 60 * 24 * 30), // 30 days
+      httpOnly: true // not accessible by JavaScript
+    });
+  }
+
+  // Send response
+  const pixelImage = Buffer.from(PIXEL_IMAGE_BASE64, 'base64')
   res.writeHead(200, {
-    'Content-Type': 'image/png',
-    'Content-Length': PIXEL_IMAGE.length
+    'Content-Type': 'image/gif',
+    'Content-Length': pixelImage.length
   })
-  res.end(PIXEL_IMAGE)
+  res.end(pixelImage)
 })
 
 async function startServer() {
   const client = new MongoClient(MONGO_URL)
   log('connecting to database...', { MONGO_URL })
   await client.connect()
-  const db = client.db()
+  db = client.db()
 
+  // const TEST_DATA = [
+  //   { timestamp: new Date('2013-09-01 09:00:00'), url: '/contact.html', userId: 12345 },
+  //   { timestamp: new Date('2013-09-01 09:00:00'), url: '/contact.html', userId: 12346  },
+  //   { timestamp: new Date('2013-09-01 10:00:00'), url: '/contact.html', userId: 12345  },
+  //   { timestamp: new Date('2013-09-01 10:01:00'), url: '/about.html', userId: 12347  },
+  //   { timestamp: new Date('2013-09-01 11:00:00'), url: '/contact.html', userId: 12347  },
+  // ]
   // for (const trackingEvent of TEST_DATA) {
   //   log('creating test data', { trackingEvent })
   //   await db.collection('trackingEvents').insertOne(trackingEvent)
   // }
-
-  const result = await db.collection('trackingEvents').aggregate([
-    // { $match: {} }, // TODO: date range
-    { $group: { _id: { timestamp: '$timestamp', url: '$url' }, pageViews: { $sum: 1 } } },
-  ], { allowDiskUse: true }).toArray()
-  // const result = await db.collection('trackingEvents').find({}).toArray()
-  log('query result', { result })
+  // const result = await db.collection('trackingEvents').aggregate([
+  //   // { $match: {} }, // TODO: date range
+  //   { $group: { _id: { timestamp: '$timestamp', url: '$url' }, pageViews: { $sum: 1 } } },
+  // ], { allowDiskUse: true }).toArray()
+  // // const result = await db.collection('trackingEvents').find({}).toArray()
+  // log('query result', { result })
 
   app.listen(port, () => {
     console.log(`tracking-pixel app listening on port ${port}`)
