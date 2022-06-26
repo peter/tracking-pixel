9be6515870c7944ec4b3850ec56da8d0301f214a
Peter Marklund <peter@marklunds.com>
2022-06-26 13:04:40 +0200

    refactor: move CLI command definition into script

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,11 @@ const { Command } = require('commander');
 const trackingReport = require('./trackingReport');
 
 async function main() {
-  const command = new Command();
+  const program = new Command();
 
-  command
-    .command('trackingReport')
-    .description('Get CSV report from /trackingReport endpoint')
-    .action(trackingReport.action)
-    .requiredOption('--from <from>', 'from timestamp or date, i.e. 2022-06-24T20:16:00.000Z or 2022-06-24')
-    .requiredOption('--to <to>', 'to timestamp or date, i.e. 2022-06-25T20:16:00.000Z or 2022-06-25')
-    .option('--baseUrl <baseUrl>', 'base URL of tracking API', 'http://localhost:3000')
+  program.addCommand(trackingReport.command)
 
-    await command.parseAsync(process.argv);
+  await program.parseAsync(process.argv);
 }
 
 main();
